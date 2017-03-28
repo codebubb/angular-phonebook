e982fee43b89d8b276798106f9168117bfb3282a
James Bubb <contact@jamesbubb.uk>
2017-03-28 12:12:57 +0100

    Add random users

--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
 var app = angular.module('AngularPhonebook', []);
 
-app.controller('HomeCtrl', ['$scope', function($scope){
+app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
   $scope.title = "Angular Phonebook";
+
+  $http({
+    method: 'GET',
+    url: 'https://randomuser.me/api/?results=20'
+  }).then( function successCallback(response){
+    $scope.users = response.data.results;
+  });
 }]);
