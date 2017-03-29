e7ee96d5143d1c393bea0bdf3b9915e05ee6a223
James Bubb <contact@jamesbubb.uk>
2017-03-29 11:31:47 +0100

    Abandon custom directive, move popup to controller

--- public/app.js
+++ public/app.js
@@ -1,7 +1,49 @@
 var app = angular.module('AngularPhonebook', []);
 
+app.directive('modalPop', function($interpolate){
+  return {
+    scope: {},
+    link: function(scope, element, attrs){
+
+      element.bind('click', function(){
+        var fn = $interpolate(element[0].dataset.user);
+        scope.currentUser = fn(scope.$parent);
+        // scope.currentUser = element[0].dataset.user;
+        document.querySelector('.user-info').style.display = 'block';
+          setTimeout(function(){
+            document.querySelector('.user-info').style.opacity = 1;
+          }, 50);
+
+        console.log(scope);
+
+      })
+      // })
+
+      // });
+      // anchor.on('click', function(event){
+      //   console.log(anchor[0]);
+      //
+      //   // scope.currentUser = anchor[0].dataset.user;
+      //   document.querySelector('.user-info').style.display = 'block';
+      //   setTimeout(function(){
+      //     document.querySelector('.user-info').style.opacity = 1;
+      //   }, 50);
+      // });
+    }
+  }
+});
+
 app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
   $scope.title = "Angular Phonebook";
+  $scope.currentUser = {name: {first: "james"}};
+
+  $scope.modalPopup = function(user_number){
+    $scope.currentUser = $scope.users[user_number];
+    document.querySelector('.user-info').style.display = 'block';
+      setTimeout(function(){
+        document.querySelector('.user-info').style.opacity = 1;
+      }, 50);
+  }
 
   $http({
     method: 'GET',
