9bb8940065b19f7c5b5beacaf022ad0b3a5e076d
James Bubb <contact@jamesbubb.uk>
2017-03-29 12:44:04 +0100

    Remove custom directive

--- public/app.js
+++ public/app.js
@@ -1,38 +1,5 @@
 var app = angular.module('AngularPhonebook', []);
 
-app.directive('modalPop', function($interpolate){
-  return {
-    scope: {},
-    link: function(scope, element, attrs){
-
-      element.bind('click', function(){
-        var fn = $interpolate(element[0].dataset.user);
-        scope.currentUser = fn(scope.$parent);
-        // scope.currentUser = element[0].dataset.user;
-        document.querySelector('.user-info').style.display = 'block';
-          setTimeout(function(){
-            document.querySelector('.user-info').style.opacity = 1;
-          }, 50);
-
-        console.log(scope);
-
-      })
-      // })
-
-      // });
-      // anchor.on('click', function(event){
-      //   console.log(anchor[0]);
-      //
-      //   // scope.currentUser = anchor[0].dataset.user;
-      //   document.querySelector('.user-info').style.display = 'block';
-      //   setTimeout(function(){
-      //     document.querySelector('.user-info').style.opacity = 1;
-      //   }, 50);
-      // });
-    }
-  }
-});
-
 app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
   $scope.title = "Angular Phonebook";
 
